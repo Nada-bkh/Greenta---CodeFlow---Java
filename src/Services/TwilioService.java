b23daa24438c154f325d3ad7d760bbf57793a07d
Nada-bkh <nada.benkhlifa@esprit.tn>
2024-03-24 15:50:40 +0100

    unfinished password reset (removed Twilio)

--- src/Services/TwilioService.java
+++
@@ -1,8 +0,0 @@
-package Services;
-
-
-public class TwilioService {
-    public static final String ACCOUNT_SID = "ACde8fd004ff87ce787de4959d5ba96295";
-    public static final String AUTH_TOKEN = "7fc6f4665277acfc6bf39ba4ec181e98";
-
-}
