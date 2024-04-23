df09ef9b897ed7c66fac9f2ebaf4ff98a3ca57dc
Laarab <mohamedarbi.zaghouani@esprit.tn>
2024-04-23 22:24:44 +0200

    yo

---
+++ src/main/java/Greenta/Controller/JobController.java
@@ -0,0 +1,17 @@
+package Greenta.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class JobController {
+
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    void sayHello(ActionEvent event) {
+
+    }
+
+}
