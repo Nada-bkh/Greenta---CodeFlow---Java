fc1258ee7a6d53cd850663e5123e0dcc93f891a5
Laarab <mohamedarbi.zaghouani@esprit.tn>
2024-04-24 23:10:36 +0200

    hi again

--- src/main/java/Greenta/Controller/HelloController.java
+++ src/main/java/Greenta/Controller/HelloController.java
@@ -1,14 +1,65 @@
 package greenta.Controller;
 
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button btClear;
+
+    @FXML
+    private Button btDelete;
+
+    @FXML
+    private Button btSave;
+
+    @FXML
+    private Button btUpdate;
+
+    @FXML
+    private MenuButton btUploadimg;
+
+    @FXML
+    private TableView<?> table;
+
+    @FXML
+    private TextField tfOrg;
+
+    @FXML
+    private TextField tfStartDate;
+
+    @FXML
+    private TextField tfTitle;
+
+    @FXML
+    void DeleteJob(ActionEvent event) {
+
     }
+
+    @FXML
+    void UpdateJob(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clearFiel(ActionEvent event) {
+
+    }
+
+    @FXML
+    void createJob(ActionEvent event) {
+
+    }
+
+    @FXML
+    void uploadimg(ActionEvent event) {
+
+    }
+
 }
